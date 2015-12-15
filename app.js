23db897585780b5a930655184f2973a7caa100db
Saeid Ziaei <Saeid Ziaei>
2015-12-15 17:52:55 +1100

    email verification and more

--- app.js
+++ app.js
@@ -22,8 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // this is just a comment to test git setup. more
 var dbConfig = require('./db.js');
-var mangoose = require('mongoose');
-mangoose.connect(dbConfig.url);
+    
+var mongoose = require('mongoose');
+var nev = require('email-verification')(mongoose);
+
+mongoose.connect(dbConfig.url);
+
+var User = require('./models/user.js');
+
+
+
 
 // Configuring Passport
 var passport = require('passport');
@@ -38,9 +46,61 @@ app.use(passport.session());
 var flash = require('connect-flash');
 app.use(flash());
 
+var i18n = require('i18next');
+i18n.init({
+  ns: {
+    namespaces: ['ns.common', 'ns.special'],
+    defaultNs: 'ns.special'
+  },
+  resSetPath: '/public/locales/__lng__/new.__ns__.json',
+  saveMissing: true,
+  debug: true,
+  sendMissingTo: 'fallback',
+  // preload: ['en', 'de'],
+  preload: ['en'],
+  detectLngFromPath: 0,
+  ignoreRoutes: ['img/', 'img', 'img/', '/img/', 'css/', 'i18next/']
+}, function(t) {
+
+  console.log('i18n is initialized.');
+
+  i18n.addRoute('/:lng', ['en', 'de'], app, 'get', function(req, res) {
+    console.log('SEO friendly route ...');
+    res.render('index');
+  });
+
+  i18n.addRoute('/:lng/route.imprint', ['en', 'de'], app, 'get', function(req, res) {
+    console.log("localized imprint route");
+    res.render('imprint');
+  });
+
+});
+app.use(i18n.handle); // have i18n befor app.router
+
+i18n.registerAppHelper(app)
+  .serveClientScript(app)
+  .serveDynamicResources(app)
+  .serveMissingKeyRoute(app);
+
+i18n.serveWebTranslate(app, {
+  i18nextWTOptions: {
+    languages: ['de-DE', 'en-US', 'dev'],
+    namespaces: ['ns.common', 'ns.special'],
+    resGetPath: "locales/resources.json?lng=__lng__&ns=__ns__",
+    resChangePath: 'locales/change/__lng__/__ns__',
+    resRemovePath: 'locales/remove/__lng__/__ns__',
+    fallbackLng: "dev",
+    dynamicLoad: true
+  }
+});
+
+
+
 // Initialize Passport
 var initPassport = require('./passport/init');
 initPassport(passport);
+
+
 
 var routes = require('./routes/index')(passport);
 app.use('/', routes);
@@ -51,6 +111,7 @@ app.use(function (req, res, next) {
   next(err);
 });
 
+
 var Bitstamp = require('bitstamp');
 var bitstamp = new Bitstamp;
 
@@ -58,6 +119,11 @@ var bitstamp = new Bitstamp;
 
 
 // update ticker every minute
+  bitstamp.ticker(function (err, data) {
+    console.log(data);
+    app.locals.bitstamp = data;
+  });
+/*
 var minutes = .1, interval = minutes * 60 * 1000;
 setInterval(function () {
   bitstamp.ticker(function (err, data) {
@@ -65,7 +131,7 @@ setInterval(function () {
     app.locals.bitstamp = data;
   });
 }, interval);
-
+*/
 
 // error handlers
 
@@ -90,6 +156,7 @@ app.use(function (err, req, res, next) {
     error: {}
   });
 });
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mongoos = require('mongoose');
+var User = require('../models/user');
+
+
+var nev = require('email-verification')(mongoos);
+nev.configure({
+    verificationURL: 'http://localhost:3000/email-verification/${URL}',
+    persistentUserModel: User,
+    tempUserCollection: 'coinava_tempusers',
+
+    transportOptions: {
+		host: 'gator2023.hostgator.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: 'dev@coinava.com',
+			pass: 'As!12345'
+		}
+    },
+    verifyMailOptions: {
+        from: 'Do Not Reply <dev@coinava.com>',
+        subject: 'Please confirm account',
+        html: 'Click the following link to confirm your account:</p><p>${URL}</p>',
+        text: 'Please confirm your account by clicking the following link: ${URL}'
+    }
+});
+nev.generateTempUserModel(User);
 
 var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler 
@@ -11,11 +38,15 @@ var isAuthenticated = function (req, res, next) {
 	res.redirect('/');
 }
 
-module.exports = function(passport){
+module.exports = function (passport) {
+
+	router.get('/str', function(req, res) {
+		res.send('locale: ' + req.locale + '<br /> key app.name -> ' + req.i18n.t('app.name'));
+	});
 
 	/* GET login page. */
-	router.get('/', function(req, res) {
-    	// Display the Login page with any flash message, if any
+	router.get('/', function (req, res) {
+		// Display the Login page with any flash message, if any
 		res.render('index', { message: req.flash('message') });
 	});
 
@@ -23,31 +54,90 @@ module.exports = function(passport){
 	router.post('/login', passport.authenticate('login', {
 		successRedirect: '/home',
 		failureRedirect: '/',
-		failureFlash : true  
+		failureFlash: true
 	}));
 
 	/* GET Registration Page */
-	router.get('/signup', function(req, res){
-		res.render('register',{message: req.flash('message')});
+	router.get('/signup', function (req, res) {
+		res.render('register', { message: req.flash('message') });
 	});
 
 	/* Handle Registration POST */
-	router.post('/signup', passport.authenticate('signup', {
-		successRedirect: '/home',
-		failureRedirect: '/signup',
-		failureFlash : true  
-	}));
+	router.post('/signup', function (req, res) {
+		var newUser = new User({
+			email: req.body.email,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			password: req.body.password,
+			username: req.body.username
+		});
+		nev.createTempUser(newUser, function (err, newTempUser) {
+			if (err) {
+				return res.status(404).send('ERROR: creating temp user FAILED');
+			}
+
+			// new user created
+			if (newTempUser) {
+				var URL = newTempUser[nev.options.URLFieldName];
+
+				nev.sendVerificationEmail(newTempUser.email, URL, function (err, info) {
+					if (err) {
+						return res.status(404).send('ERROR: sending verification email FAILED');
+					}
+					res.json({
+						msg: 'An email has been sent to you. Please check it to verify your account.',
+						info: info
+					});
+				});
+
+				// user already exists in temporary collection!
+			} else {
+				res.json({
+					msg: 'You have already signed up. Please check your email to verify your account.'
+				});
+			}
+		});
+
+		passport.authenticate('signup', {
+			successRedirect: '/home',
+			failureRedirect: '/signup',
+			failureFlash: true
+		});
+	});
 
 	/* GET Home Page */
-	router.get('/home', isAuthenticated, function(req, res){
+	// router.get('/home', isAuthenticated, function (req, res) {
+	router.get('/home', function (req, res) {
 		res.render('home', { user: req.user });
 	});
 
 	/* Handle Logout */
-	router.get('/signout', function(req, res) {
+	router.get('/signout', function (req, res) {
 		req.logout();
 		res.redirect('/');
 	});
+
+	// user accesses the link that is sent
+	router.get('/email-verification/:URL', function(req, res) {
+	var url = req.params.URL;
+	
+	nev.confirmTempUser(url, function(err, user) {
+		if (user) {
+		nev.sendConfirmationEmail(user.email, function(err, info) {
+			if (err) {
+				return res.status(404).send('ERROR: sending confirmation email FAILED');
+			}
+			res.json({
+			msg: 'CONFIRMED!',
+			info: info
+			});
+		});
+		} else {
+		return res.status(404).send('ERROR: confirming temp user FAILED');
+		}
+	});
+	});
+
 
 	return router;
 }
